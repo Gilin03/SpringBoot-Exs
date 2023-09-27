62e0e06e97de978b2208f2dc55b2a1910f9ed34c
gilin03 <tb081212@naver.com>
2023-09-27 12:46:50 +0900

    ch04

---
+++ ch4-test/ch04-test/src/main/java/com/example/ch04test/Ch04TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch04test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch04TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch04TestApplication.class, args);
+    }
+
+}
