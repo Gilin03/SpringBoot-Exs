9b7943037974a52df74fdd4761590af3e5f3f589
gilin03 <tb081212@naver.com>
2023-09-27 12:25:10 +0900

    ch04

---
+++ ch4-test/ch04-test/src/main/java/com/example/ch04test/constant/ItemSellStatus.java
@@ -0,0 +1,5 @@
+package com.example.ch04test.constant;
+
+public enum ItemSellStatus {
+    SELL, SOLD_OUT
+}
---
+++ ch4-test/ch04-test/src/main/java/com/example/ch04test/constant/Role.java
@@ -0,0 +1,5 @@
+package com.example.ch04test.constant;
+
+public enum Role {
+    USER, ADMIN
+}
